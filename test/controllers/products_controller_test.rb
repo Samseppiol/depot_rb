5f9d8f0c64e0d59ccf5fa63e81e83fd9ae2467dc
Jack Hage <jh@Jacks-MacBook-Pro.local>
2017-01-08 16:03:30 +1100

    finished testing validation.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @update = {
+      title: 'PC-F8',
+      description: '8 Way Fuse Lock Module',
+      image_url: 'PC-F8.jpg',
+      price: 72.00
+  }
   end
 
   test "should get index" do
@@ -17,7 +23,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post products_url, params: {product: @update }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,8 +40,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
-    assert_redirected_to product_url(@product)
+  patch product_url(@product), params: { product: @update }
+  assert_redirected_to product_url(@product)
   end
 
   test "should destroy product" do
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+test "product attributes must not be empty" do
+  product = Product.new
+  assert product.invalid?
+  assert product.errors[:title].any?
+  assert product.errors[:description].any?
+  assert product.errors[:image_url].any?
+  assert product.errors[:price].any?
+ end
+
+ test "product price must be positive" do
+   product = Product.new(
+   title: "PC-F10",
+   description: "10 way lock module",
+   image_url: "ccb.jpg")
+
+   product.price = -1
+   assert product.invalid?
+   assert_equal ["must be greater than or equal to 0.01"],
+   product.errors[:price]
+
+   product.price = 0
+   assert product.invalid?
+   assert_equal ["must be greater than or equal to 0.01"],
+   product.errors[:price]
+
+product.price = 1
+assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(
+    title: "PC-F12",
+    description: "12 Way Lock Module",
+    price: 1,
+    image_url: image_url)
+  end
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.JPG FRED.Jpg
+    http://a.b.c/x/y/z/fred.gif}
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok.each do |name|
+      assert new_product(name) .valid?, "#{name} shouldn't be invalid"
+  end
+bad.each do |name|
+assert new_product(name). invalid?, "#{name} shouldn't be valid"
+  end
+ end
 end
