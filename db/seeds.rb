abddeb0a6ab20e7177e25ff74d29f5fb850e8555
Jack Hage <jh@Jacks-MacBook-Pro.local>
2017-01-08 14:27:19 +1100

    First push of this branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Product.delete_all
 Product.create(title: 'PC-F8',
   description:
     %{<p>
-      <em>8 Fuse Lock Module </em>
+      <em>8 Way Fuse Lock Module </em>
 
       </p>},
   image_url: 'PC-F8.jpg',
